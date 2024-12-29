d95e922d1523aee71214745e752fc9cdd6174414
Adrien Maillet González <122007907+adrien-maillet-gonzalez@users.noreply.github.com>
2024-12-29 18:57:10 +0100

    plot good bor

--- project_matlab/main.m
+++ project_matlab/main.m
@@ -199,15 +199,15 @@ axis padded;
 
 
 exportgraphics(gcf,'plots/SER_CPlen_TEST.png','Resolution',600)
-
-figure(1);
+%%
+figure(2);
 CP_len_list = [2, 4, 8, 16, 32, 40, 64, 80, 100, 128];
-spectral_efficiency_list = [14, 16, 11.8, 12.1, 2, 1.5, 1.5, 0, 0, 0].*10^-3;
+spectral_efficiency_list = [14, 16, 11.8, 12.1, 2, 1.5, 0.1, 0, 0, 0].*10^5;
 
 semilogy(CP_len_list, 1+spectral_efficiency_list, '.', 'MarkerSize',20);
 title("Spectral Efficiency vs Cyclic Prefix lenght");
 xlabel("Cyclic Prefix length");
-ylabel("Symbol Error Rate, log(1+x)");
+ylabel("Symbol Error Rate, log(1+x\times10^5)");
 xticks(CP_len_list);
 yline(1, '-.')
 axis padded;
